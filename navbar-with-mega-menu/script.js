31add2d36c0960027fcb6f44b8d31247b0836a84
Sam(Mohsen) Kamrani <m.kamrani@hotmail.com>
2024-01-03 15:28:36 +1100

    add navbar with mega menu (#1)

---
+++ navbar-with-mega-menu/script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+  const navList = document.querySelector(".nav-list");
+  navList.classList.toggle("active"); //adds and removes the class active from the list of classes
+}
